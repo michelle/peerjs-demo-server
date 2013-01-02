723abc4a777035a37ad198aa087f71117a4603c8
Michelle Bu <michelle@michellebu.com>
2013-01-01 19:38:39 -0800

    heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var fs = require('fs');
+var peer = require('peer');
+var app =  express.createServer();
+
+ps = new peer.PeerServer({ debug: true });
+
+// Initialize main server
+/*app.use(express.bodyParser());
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+app.listen(80);
+*/
+
